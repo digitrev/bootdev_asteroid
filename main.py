1e23c27f5390035164ad97886eb08b012efda2be
digitrev <digitrev@gmail.com>
2024-12-31 20:57:39 -0500

    Asteroids in fields, even

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,12 +13,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    updateable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    Player.containers = (updateable, drawable)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -24,7 +30,7 @@ def main():
                 return
         
         screen.fill("black")
-        for g in updateable:
+        for g in updatable:
             g.update(dt)
         for g in drawable:
             g.draw(screen)
